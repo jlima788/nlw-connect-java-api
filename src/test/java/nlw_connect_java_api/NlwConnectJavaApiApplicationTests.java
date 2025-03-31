c732ab9ea81d74fba05e44f975610b292ae78646
Everaldo Lima <jlima788@yahoo.com.br>
2025-03-30 22:33:10 -0300

    Initial commit

---
+++ src/test/java/nlw_connect_java_api/NlwConnectJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nlw_connect_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwConnectJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
